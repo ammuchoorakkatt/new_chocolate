3f851bc7fcdb5ecf3b852600b553e128a0252f96
ammuchoorakkatt <ammuchoorakkatt@gmail.com>
2022-06-18 15:19:14 +0530

    added

---
+++ chocohutpro/choco_app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name = 'choco_app'
+
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('register',views.register,name='register'),
+    path('login',views.login,name='login'),
+    path('logout',views.logout,name='logout'),
+    path('shop',views.shop,name='shop'),
+    path('confirm', views.confirm, name='confirm'),
+
+]
